fc63304db775e36c3e50a072883884223a98cf1e
bolek <bolek@tolemi.com>
2025-03-07 14:51:54 -0500

    fix(transport) fix error handling on handle_init
    Waiting for this or similar to get merged
    https://github.com/absinthe-graphql/absinthe_graphql_ws/pull/18

--- lib/absinthe/graphql_ws/transport.ex
+++ lib/absinthe/graphql_ws/transport.ex
@@ -126,8 +126,11 @@ defmodule Absinthe.GraphqlWS.Transport do
         {:ok, payload, socket} ->
           {:reply, :ok, {:text, Message.ConnectionAck.new(payload)}, %{socket | initialized?: true}}
 
-        {:error, payload, socket} ->
-          {:reply, :ok, {:text, Message.Error.new(payload)}, socket}
+        {:error, {code, message}, socket} ->
+          close(code, message, socket)
+
+        {:error, message, socket} ->
+          close(4401, message, socket)
       end
     else
       {:reply, :ok, {:text, Message.ConnectionAck.new()}, %{socket | initialized?: true}}
